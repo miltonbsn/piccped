937e4576d6c16d702a76896caeba15cf241ca4b8
Arthur Mota <arthur.nascimento@mercadolivre.com>
2022-10-14 15:16:58 -0300

    Update labels and question component logic

--- src/components/generalCase/index.jsx
+++ src/components/generalCase/index.jsx
@@ -14,10 +14,6 @@ export default class GeneralCase extends Component {
       <div class={style.wrapper}>
         <IconTitle icon="CG" label="Caso clínico geral" largeIcon class={style.title}/>
 
-        <div class={style.btnPlay}>
-          <BtnPlay icon="../../assets/svg/headNurse.svg" label="Ouvir" play={ () => this.props.play() } />
-        </div>
-
         <div class={style.content}>
           <p>Arthur, 2 anos, foi submetido a passagem de PICC há um dia devido a fragilidade venosa e necessidade de antibioticoterapia por 10 dias (Ceftriaxone) e corticoterapia (Dexametasona) por 5 dias. O PICC de 3 fr foi inserido com sucesso na terceira tentativa de punção, em veia basílica na fossa antecubital direita. O comprimento do cateter estimado foi de 30 cm, tendo progressão dos 30 cm. No momento da inserção a enfermeira realizou o teste para confirmação do refluxo sanguíneo, confirmando-o e a infusão de soro fisiológico foi realizada sem resistência. Após isso, o raio-x evidenciou o posicionamento da ponta do cateter na junção cavoatrial.</p>
           <p>No plantão do dia seguinte, você, enfermeiro da Unidade, fez a avaliação geral de Arthur e agora precisa avaliar os aspectos relacionados ao PICC.</p>
