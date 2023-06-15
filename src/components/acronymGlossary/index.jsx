b436cc31f9827323484b2a8171edf6cb56f7607b
Arthur Mota <arthur.nascimento@mercadolivre.com>
2023-06-14 21:31:53 -0300

    Create Acronym Glossary page

---
+++ src/components/acronymGlossary/index.jsx
@@ -0,0 +1,41 @@
+/* eslint-disable react/sort-comp */
+import { h, Component } from 'preact';
+import { IconTitle } from '../common/titles/IconTitle';
+import style from './style.less';
+
+export default class AcronymGlossary extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			definitions: [
+				'ASD - Adhesive securement devices',
+				'CLABSI - Central line-associated bloodstream infection',
+				'CVAD -  Central vascular access device',
+				'ISD - Integrated securement device',
+				'PICC - Peripherally inserted central catheter',
+				'PIVC - Peripheral intravenous catheter',
+				'SASS - Subcutaneous anchor securement system',
+				'TA - Tissue adhesive'
+			]
+		};
+	}
+
+	showModal(state) {
+		this.props.showModal(state);
+	}
+
+	render() {
+		return (
+			<div class={style.wrapper}>
+				<IconTitle icon="AG" label="Acronym glossary" class={style.title} />
+
+				<div class={style.groupButtons}>
+					{this.state.definitions.map(definition => (
+						<p>{definition}</p>
+					))}
+				</div>
+			</div>
+		);
+	}
+}
